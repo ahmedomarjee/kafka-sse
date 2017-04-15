01e6445843e82d165995e8a0f8ea5f30fcbb1f8e
Conor Mongey <conor@mongey.net>
2017-04-16 00:03:13 +0100

    Allow the user to select topic and offset

--- sse.go
+++ sse.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -14,29 +17,30 @@ import (
 //     $ go run sse.go
 
 type Broker struct {
-
 	// Events are pushed to this channel by the main events-gathering routine
-	Notifier chan []byte
-
+	Notifier chan *sarama.ConsumerMessage
 	// New client connections
-	newClients chan chan []byte
-
+	newClients chan KafkaUpdateChannel
 	// Closed client connections
-	closingClients chan chan []byte
-
+	closingClients chan KafkaUpdateChannel
 	// Client connections registry
-	clients map[chan []byte]bool
+	clients map[KafkaUpdateChannel]bool
+}
+
+type KafkaUpdateChannel struct {
+	KafkaCh chan *sarama.ConsumerMessage
+	Topic   string
+	Offset  int64
 }
 
 func NewServer() (broker *Broker) {
 	// Instantiate a broker
 	broker = &Broker{
-		Notifier:       make(chan []byte, 1),
-		newClients:     make(chan chan []byte),
-		closingClients: make(chan chan []byte),
-		clients:        make(map[chan []byte]bool),
+		Notifier:       make(chan *sarama.ConsumerMessage, 1),
+		newClients:     make(chan KafkaUpdateChannel),
+		closingClients: make(chan KafkaUpdateChannel),
+		clients:        make(map[KafkaUpdateChannel]bool),
 	}
-
 	// Set it running - listening and broadcasting events
 	go broker.listen()
 
@@ -46,7 +50,6 @@ func NewServer() (broker *Broker) {
 func (broker *Broker) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	// Make sure that the writer supports flushing.
-	//
 	flusher, ok := rw.(http.Flusher)
 
 	if !ok {
@@ -59,16 +62,32 @@ func (broker *Broker) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Set("Connection", "keep-alive")
 	rw.Header().Set("Access-Control-Allow-Origin", "*")
 
+	topic := req.URL.Query().Get("topic")
+	offsetStr := req.URL.Query().Get("offset")
+
+	offset := sarama.OffsetOldest
+	userSuppliedOffset, err := strconv.Atoi(offsetStr)
+
+	if err == nil {
+		offset = int64(userSuppliedOffset)
+	}
+
 	// Each connection registers its own message channel with the Broker's connections registry
-	messageChan := make(chan []byte)
+	messageChan := make(chan *sarama.ConsumerMessage)
 
 	// Signal the broker that we have a new connection
-	broker.newClients <- messageChan
+	KafkaChannel := KafkaUpdateChannel{
+		KafkaCh: messageChan,
+		Topic:   topic,
+		Offset:  offset,
+	}
+
+	broker.newClients <- KafkaChannel
 
 	// Remove this client from the map of connected clients
 	// when this handler exits.
 	defer func() {
-		broker.closingClients <- messageChan
+		broker.closingClients <- KafkaChannel
 	}()
 
 	// Listen to connection close and un-register messageChan
@@ -76,19 +95,36 @@ func (broker *Broker) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	go func() {
 		<-notify
-		broker.closingClients <- messageChan
+		broker.closingClients <- KafkaChannel
 	}()
 
 	for {
 
-		// Write to the ResponseWriter
-		// Server Sent Events compatible
-		fmt.Fprintf(rw, "data: %s\n\n", <-messageChan)
-
+		// Write to the ResponseWriter Server Sent Events compatible
 		// Flush the data immediatly instead of buffering it for later.
-		flusher.Flush()
+		thing := <-messageChan
+		json, err := json.Marshal(toCT(thing))
+		if err == nil {
+			fmt.Fprintf(rw, "data: %s\n\n", json)
+			flusher.Flush()
+		}
 	}
+}
 
+type ConvertedThing struct {
+	Key       string `json:"key"`
+	Value     string `json:"value"`
+	Offset    int64  `json:"offset"`
+	Partition int32  `json:"partition"`
+}
+
+func toCT(c *sarama.ConsumerMessage) ConvertedThing {
+	return ConvertedThing{
+		Key:       string(c.Key[:]),
+		Value:     string(c.Value[:]),
+		Offset:    c.Offset,
+		Partition: c.Partition,
+	}
 }
 
 func (broker *Broker) listen() {
@@ -99,7 +135,7 @@ func (broker *Broker) listen() {
 			// A new client has connected.
 			// Register their message channel
 			broker.clients[s] = true
-			sendKafkaMessages(s)
+			BackfillKafkaMessages(s)
 			log.Printf("Client added. %d registered clients", len(broker.clients))
 		case s := <-broker.closingClients:
 
@@ -111,20 +147,29 @@ func (broker *Broker) listen() {
 
 			// We got a new event from the outside!
 			// Send event to all connected clients
-			for clientMessageChan, _ := range broker.clients {
-				clientMessageChan <- event
+			for clientMessageChan := range broker.clients {
+				clientMessageChan.KafkaCh <- event
 			}
 		}
 	}
 
 }
 
-func sendKafkaMessages(ch chan []byte) {
-	kafkaBrokerList := "localhost:9092"
-	topic := "example"
-	bufferSize := 256
+func GetENV(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
 
-	initialOffset := sarama.OffsetOldest
+func BackfillKafkaMessages(s KafkaUpdateChannel) {
+	ch := s.KafkaCh
+	kafkaBrokerList := GetENV("KAFKA_BROKERS", "localhost:9092")
+	topic := GetENV("TOPIC_NAME", s.Topic)
+	bufferSize := 1
+
+	initialOffset := s.Offset
 
 	c, err := sarama.NewConsumer(strings.Split(kafkaBrokerList, ","), nil)
 
@@ -132,6 +177,7 @@ func sendKafkaMessages(ch chan []byte) {
 		log.Printf("Kafka connection error %s", err)
 	}
 	partitionList, err := c.Partitions(topic)
+	log.Printf("Connected to %s , starting at %d", topic, initialOffset)
 
 	if err != nil {
 		log.Println("error getting partions %s", err)
@@ -164,14 +210,16 @@ func sendKafkaMessages(ch chan []byte) {
 	}
 	go func() {
 		for msg := range messages {
-			log.Printf("Kafka Message: %s", msg.Value)
-
-			ch <- []byte(msg.Value)
+			ch <- msg
 		}
 	}()
-
 }
-func main() {
+
+func StreamTopicHandler(w http.ResponseWriter, r *http.Request) {
 	broker := NewServer()
-	log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", broker))
+	broker.ServeHTTP(w, r)
+}
+
+func main() {
+	log.Fatal("HTTP server error: ", http.ListenAndServe("0.0.0.0:4000", http.HandlerFunc(StreamTopicHandler)))
 }
